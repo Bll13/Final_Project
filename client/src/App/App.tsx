b988055d1e449ce4f202cc9c46209a7a3fb710d1
Bll13 <tokbll13@gmail.com>
2023-10-13 19:48:52 +0300

    MainDon

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -6,16 +6,15 @@ import { Route, Routes } from 'react-router-dom';
 import Registration from '../features/authc/Registration';
 
 import Login from '../features/authc/Login';
+import MainPage from '../features/mainPage/MainPage';
 
 function App(): JSX.Element {
   return (
     <Routes>
+      <Route path='/' element={<MainPage/>} />
       <Route path="/auth" element={<Registration />} />
       <Route path="/log" element={<Login />} />
     </Routes>
-    // <div>
-    //   <Registration/>
-    // </div>
   );
 }
 
