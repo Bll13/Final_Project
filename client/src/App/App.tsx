f983482df133b70e88601e31e15b5614cba6e899
Bll13 <tokbll13@gmail.com>
2023-10-13 14:11:20 +0300

    123

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
 import './App.css';
+import MainPage from '../features/mainPage/MainPage';
 
 function App(): JSX.Element {
-  return <div className="App">App</div>;
+  return <div className="App">
+    <MainPage/>
+    </div>;
 }
 
 export default App;
---
+++ client/src/features/mainPage/MainPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function MainPage():JSX.Element {
+
+
+
+
+  return (
+    <div className='mainPage'>
+      mainPage
+    </div>
+  )
+}
+
+export default MainPage
