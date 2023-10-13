679a22b80fd4be3f0a7ba58fbb359a3f115aff43
Bll13 <tokbll13@gmail.com>
2023-10-13 17:59:25 +0300

    mainStyle

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
 import './App.css';
+
 import { Route, Routes } from 'react-router-dom';
 import Registration from '../features/authc/Registration';
 import MainPage from '../features/mainPage/MainPage';
 
 function App(): JSX.Element {
   return (
-    <Routes>
-      <Route path='/auth' element={<Registration/>}/>
+    // <Routes>
+      // {/* <Route path='/auth' element={<Registration/>}/> */}
       <MainPage/>
       
-    </Routes>
+    // {/* </Routes> */}
     
   )
 }
--- client/src/features/mainPage/MainPage.tsx
+++ client/src/features/mainPage/MainPage.tsx
@@ -1,15 +1,130 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react';
 
-function MainPage():JSX.Element {
+import { Dialog, Transition } from '@headlessui/react';
+import { XMarkIcon } from '@heroicons/react/24/outline';
+import './MainPage.css';
 
+function MainPage(): JSX.Element {
+  const [open, setOpen] = useState(true);
 
-
+  const links = [{ name: 'Войти', href: '' }];
 
   return (
-    <div>
-      mainPage
+    <div className="mainPage">
+      <div className="relative isolate overflow-hidden  py-24 sm:py-32">
+        <div
+          className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
+          aria-hidden="true"
+        >
+          <div
+            className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+            style={{
+              clipPath:
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+            }}
+          />
+        </div>
+        <div
+          className="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu"
+          aria-hidden="true"
+        >
+          <div
+            className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+            style={{
+              clipPath:
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+            }}
+          />
+        </div>
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="jopa grid grid-cols-1 gap-x-8 gap-y-6 text-base font-semibold leading-7 text-white sm:grid-cols-2 md:flex rg:gap-x-10">
+            {links.map((link) => (
+              <a key={link.name} href={link.href}>
+                {link.name} <span aria-hidden="true">&rarr;</span>
+              </a>
+            ))}
+          </div>
+
+          <div className="mx-auto max-w-2xl lg:mx-0">
+            <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">Ceep Clean</h2>
+            <p className="mt-6 text-lg leading-8 text-gray-200">
+              Правильная утилизация отходов является неотъемлемой частью нашей ответственности перед
+              природой и будущим поколениям. Она помогает минимизировать загрязнение окружающей
+              среды, сохранять природные ресурсы и создавать более здоровую планету для всех.
+              Давайте вместе принимать меры и делать свой вклад в сохранение экологии, начиная с
+              правильной утилизации отходов.
+            </p>
+          </div>
+          <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none"></div>
+        </div>
+        <Transition.Root show={open} as={Fragment}>
+          <Dialog as="div" className="relative z-10" onClose={setOpen}>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-in-out duration-500"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in-out duration-500"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            </Transition.Child>
+
+            <div className="fixed inset-0 overflow-hidden">
+              <div className="absolute inset-0 overflow-hidden">
+                <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+                  <Transition.Child
+                    as={Fragment}
+                    enter="transform transition ease-in-out duration-500 sm:duration-700"
+                    enterFrom="translate-x-full"
+                    enterTo="translate-x-0"
+                    leave="transform transition ease-in-out duration-500 sm:duration-700"
+                    leaveFrom="translate-x-0"
+                    leaveTo="translate-x-full"
+                  >
+                    <Dialog.Panel className="pointer-events-auto relative w-screen max-w-md">
+                      <Transition.Child
+                        as={Fragment}
+                        enter="ease-in-out duration-500"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        leave="ease-in-out duration-500"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                      >
+                        <div className="absolute left-0 top-0 -ml-8 flex pr-2 pt-4 sm:-ml-10 sm:pr-4">
+                          <button
+                            type="button"
+                            className="relative rounded-md text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
+                            onClick={() => setOpen(false)}
+                          >
+                            <span className="absolute -inset-2.5" />
+                            <span className="sr-only">Close panel</span>
+                            <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                          </button>
+                        </div>
+                      </Transition.Child>
+                      <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
+                        <div className="px-4 sm:px-6">
+                          <Dialog.Title className="text-base font-semibold leading-6 text-gray-900">
+                            Panel title
+                          </Dialog.Title>
+                        </div>
+                        <div className="relative mt-6 flex-1 px-4 sm:px-6">
+                          {'/* Your content */'}
+                        </div>
+                      </div>
+                    </Dialog.Panel>
+                  </Transition.Child>
+                </div>
+              </div>
+            </div>
+          </Dialog>
+        </Transition.Root>
+      </div>
     </div>
-  )
+  );
 }
 
-export default MainPage
+export default MainPage;
